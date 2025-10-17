9e518a4b19ec3171da1f9475168730f3414d8ba3
moisesgross37 <moisesgross37@gmail.com>
2025-10-17 09:47:29 -0400

    Update server_v12.js

--- server_v12.js
+++ server_v12.js
@@ -59,7 +59,7 @@ const initializeDatabase = async () => {
     const client = await pool.connect();
     try {
         // ¡NUEVO! Añadimos esta línea para eliminar la tabla incorrecta antes de volver a crearla.
-        await client.query('DROP TABLE IF EXISTS formalized_centers;');
+        // await client.query('DROP TABLE IF EXISTS formalized_centers;');
         
         await client.query(`
             CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, nombre VARCHAR(255) NOT NULL, username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, rol VARCHAR(50) NOT NULL, estado VARCHAR(50) DEFAULT 'activo' );
